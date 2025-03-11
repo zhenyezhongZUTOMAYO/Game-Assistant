c0253c082d32664312d7c14b95831a01dcd7cac9
Camus0412 <2164298709@qq.com>
2025-03-12 01:24:27 +0800

    修正时间和坐标

--- openzzz/main.py
+++ openzzz/main.py
@@ -1,26 +1,27 @@
 import pyautogui
 import pynput
 from time import sleep
-keyboard=pynput.keyboard.Controller()
-sleep(4)
-keyboard.press(pynput.keyboard.Key['f2'])
-sleep(0.5)
-keyboard.release(pynput.keyboard.Key['f2'])
-pyautogui.click(1819,197)#(x,y)作战
-sleep(1)
-pyautogui.click(442,618)#(x,y)零号空洞
-sleep(1)
-pyautogui.click(1939,631)#(x,y)前往
-sleep(1)
-pyautogui.click(1459,826)#(x,y)确认
-sleep(1)
-pyautogui.click(1750,789)#(x,y)战线肃清
-sleep(1)
-pyautogui.click(1750,789)#(x,y)点一个buff
-sleep(1)
-pyautogui.click(2233,794)#(x,y)下一步
-sleep(1)
-pyautogui.click(2233,794)#(x,y)出战
+def openZzz():
+    keyboard=pynput.keyboard.Controller()
+    sleep(4)
+    keyboard.press(pynput.keyboard.Key['f2'])
+    sleep(0.5)
+    keyboard.release(pynput.keyboard.Key['f2'])
+    pyautogui.click(1819,197)#(x,y)作战
+    sleep(1)
+    pyautogui.click(442,618)#(x,y)零号空洞
+    sleep(1)
+    pyautogui.click(1939,631)#(x,y)前往
+    sleep(1)
+    pyautogui.click(1459,826)#(x,y)确认
+    sleep(3)
+    pyautogui.click(1750,789)#(x,y)战线肃清
+    sleep(1)
+    pyautogui.click(1750,789)#(x,y)点一个buff
+    sleep(1)
+    pyautogui.click(2274,1372)#(x,y)下一步
+    sleep(1)
+    pyautogui.click(2274,1372)#(x,y)出战
 # pyautogui.keyUp('alt')
 
 # def on_click(x, y, button, pressed):
