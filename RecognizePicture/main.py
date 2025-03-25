d092b3112aeb0a60c59351eb8a5d59645f58e0f4
Lemontree68-coder <295213423@qq.com>
2025-03-25 19:47:18 +0800

    --task==1025868 --user==王梓名

--- RecognizePicture/main.py
+++ RecognizePicture/main.py
@@ -97,4 +97,5 @@ if __name__=="__main__":
     #     sys.exit()  # 退出当前非管理员权限的进程
     # openzzz.openZzz()
     rec.ToRecognizeIfThen(rec.source_path+"Game-Assistant\\Source\\"+str(rec.resolutionRatio[0])+"GanTan.png",CommunicateToNpc())
-
+    level_system = LevelSystem()  # 创建进入下一层实例
+    level_system.start_detection()  # 开始检测入口
